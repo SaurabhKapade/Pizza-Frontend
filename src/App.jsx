6e93af7ce970daffd5bd49fb10cf518d1c272d04
Saurabh Kapade <saurabhkapade60@gmail.com>
2024-08-27 15:51:15 +0530

    added auth and addProduct pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import AddProduct from "./pages/Admin/AddProduct"
+import LoginPresentation from "./pages/Auth/LoginPresentation"
+import SignUpPresentation from "./pages/Auth/SignUpPresentation"
 import Home from "./pages/Home"
-import Layout from "./Layouts/Layout"
+import { Route, Routes } from "react-router-dom"
 function App() {
 
   return(
     <>
-      <Layout>
-        <Home/>
-      </Layout>
+      <Routes>
+        <Route path = "/" element={<Home/>}/>
+        <Route path = "/auth/signup" element={<SignUpPresentation/>}/>
+        <Route path = "/auth/login" element={<LoginPresentation/>}/>
+        <Route path = "/admin" element={<AddProduct/>}/>
+      </Routes>
     </>
   )
 }
