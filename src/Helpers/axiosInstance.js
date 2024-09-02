7ba0aa27db1c68a6e8dda843f49ae5bf1b1c480a
Saurabh Kapade <saurabhkapade60@gmail.com>
2024-09-02 14:39:58 +0530

    added page for admin

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = import.meta.env.VITE_BACKEND_URL
+console.log(import.meta.env)
 
 axiosInstance.defaults.withCredentials = true
 
